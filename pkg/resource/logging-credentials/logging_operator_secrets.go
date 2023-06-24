5982a1d8d2eae52ce7cb74c071dbaf7e52ff8ba1
Herve Nicol <12008875+hervenicol@users.noreply.github.com>
2023-06-24 16:50:16 +0200

    logging-credentials: move secret updates in a function

--- pkg/resource/logging-credentials/logging_operator_secrets.go
+++ pkg/resource/logging-credentials/logging_operator_secrets.go
@@ -48,3 +48,28 @@ func GenerateLoggingCredentialsBasicSecret(lc loggedcluster.Interface) (v1.Secre
 
 	return secret, nil
 }
+
+// Update a LoggingCredentials secret if needed
+func UpdateLoggingCredentials(loggingCredentials v1.Secret) bool {
+
+	var secretUpdated bool = false
+
+	if _, ok := loggingCredentials.Data["readuser"]; !ok {
+		loggingCredentials.Data["readuser"] = []byte("read")
+		secretUpdated = true
+	}
+	if _, ok := loggingCredentials.Data["readpassword"]; !ok {
+		loggingCredentials.Data["readpassword"] = []byte(genPassword())
+		secretUpdated = true
+	}
+	if _, ok := loggingCredentials.Data["writeuser"]; !ok {
+		loggingCredentials.Data["writeuser"] = []byte("write")
+		secretUpdated = true
+	}
+	if _, ok := loggingCredentials.Data["writepassword"]; !ok {
+		loggingCredentials.Data["writepassword"] = []byte(genPassword())
+		secretUpdated = true
+	}
+
+	return secretUpdated
+}
--- pkg/resource/logging-credentials/reconciler.go
+++ pkg/resource/logging-credentials/reconciler.go
@@ -26,64 +26,43 @@ type Reconciler struct {
 // ReconcileCreate ensures a secret exists for the given cluster.
 func (r *Reconciler) ReconcileCreate(ctx context.Context, lc loggedcluster.Interface) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
-	logger.Info("loggingCredentials secret create")
 
 	const secretNamespace = "monitoring"
 	const secretName = "logging-credentials"
 
-	// Check if secrets exist.
 	logger.Info(fmt.Sprintf("loggingcredentials checking secret %s/%s", secretNamespace, secretName))
 
-	var currentSecret v1.Secret
+	var loggingCredentialsSecret v1.Secret
 
-	// To be updated when secret is modified
-	var secretUpdated bool = false
-
-	err := r.Client.Get(ctx, types.NamespacedName{Name: secretName, Namespace: secretNamespace}, &currentSecret)
+	// Check if secrets exist / retrieve existing secret
+	err := r.Client.Get(ctx, types.NamespacedName{Name: secretName, Namespace: secretNamespace}, &loggingCredentialsSecret)
 	if err != nil {
 		if apimachineryerrors.IsNotFound(err) {
 			logger.Info("logging-credentials not found, initializing one")
 			// Create basic secret
-			currentSecret, err = GenerateLoggingCredentialsBasicSecret(lc)
-			secretUpdated = true
+			loggingCredentialsSecret, err = GenerateLoggingCredentialsBasicSecret(lc)
 		}
 		if err != nil {
 			return ctrl.Result{}, errors.WithStack(err)
 		}
 	}
 
-	// Check we have required data
-	if _, ok := currentSecret.Data["readuser"]; !ok {
-		logger.Info("loggingCredentials - adding readuser key")
-		currentSecret.Data["readuser"] = []byte("read")
-		secretUpdated = true
-	}
-	if _, ok := currentSecret.Data["readpassword"]; !ok {
-		logger.Info("loggingCredentials - Adding readpassword key")
-		currentSecret.Data["readpassword"] = []byte(genPassword())
-		secretUpdated = true
-	}
-	if _, ok := currentSecret.Data["writeuser"]; !ok {
-		logger.Info("loggingCredentials - Adding writeuser key")
-		currentSecret.Data["writeuser"] = []byte("write")
-		secretUpdated = true
-	}
-	if _, ok := currentSecret.Data["writepassword"]; !ok {
-		logger.Info("loggingCredentials - Adding writepassword")
-		currentSecret.Data["writepassword"] = []byte(genPassword())
-		secretUpdated = true
-	}
+	// update the secret's contents if needed
+	secretUpdated := UpdateLoggingCredentials(loggingCredentialsSecret)
 
+	// commit our changes
 	if secretUpdated {
 		logger.Info("loggingCredentials - Updating secret")
-		err = r.Client.Update(ctx, &currentSecret)
+		err = r.Client.Update(ctx, &loggingCredentialsSecret)
 		if apimachineryerrors.IsNotFound(err) {
 			logger.Info("loggingCredentials - Secret does not exist, creating it")
-			err = r.Client.Create(ctx, &currentSecret)
+			err = r.Client.Create(ctx, &loggingCredentialsSecret)
 		}
 		if err != nil {
 			return ctrl.Result{}, errors.WithStack(err)
 		}
+	} else {
+		logger.Info("loggingCredentials - up to date")
 	}
 
 	return ctrl.Result{}, nil
