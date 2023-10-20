a642462d7787cf137a947c36003067b67776c79c
Marie Roque <marie@giantswarm.io>
2023-10-20 15:40:51 +0200

    Merge promtail and grafana-agent toggle

---
+++ pkg/resource/apps-toggle/observability_bundle_configmap.go
@@ -0,0 +1,54 @@
+package appstoggle
+
+import (
+	"github.com/blang/semver"
+	"github.com/pkg/errors"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/yaml"
+
+	"github.com/giantswarm/logging-operator/pkg/common"
+	loggedcluster "github.com/giantswarm/logging-operator/pkg/logged-cluster"
+)
+
+type Values struct {
+	Apps map[string]app `yaml:"apps" json:"apps"`
+}
+
+type app struct {
+	Enabled bool `yaml:"enabled" json:"enabled"`
+}
+
+// GenerateObservabilityBundleConfigMap returns a configmap for
+// the observabilitybundle application to enable Promtail/GrafanaAgent.
+func GenerateObservabilityBundleConfigMap(lc loggedcluster.Interface, observabilityBundleVersion semver.Version) (v1.ConfigMap, error) {
+	promtailName := "promtail"
+	grafanaAgentName := "grafanaAgent"
+	if observabilityBundleVersion.LT(semver.MustParse("1.0.0")) {
+		promtailName = "promtail-app"
+	}
+
+	values := Values{
+		Apps: map[string]app{
+			promtailName: {
+				Enabled: true,
+			},
+			grafanaAgentName: {
+				Enabled: true,
+			},
+		},
+	}
+
+	v, err := yaml.Marshal(values)
+	if err != nil {
+		return v1.ConfigMap{}, errors.WithStack(err)
+	}
+
+	configmap := v1.ConfigMap{
+		ObjectMeta: common.ObservabilityBundleConfigMapMeta(lc),
+		Data: map[string]string{
+			"values": string(v),
+		},
+	}
+
+	return configmap, nil
+}
