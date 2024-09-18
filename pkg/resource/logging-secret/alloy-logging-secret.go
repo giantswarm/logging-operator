f6096ae0db66edeae59af343ac097f9671a886e0
Théo Brigitte <theo.brigitte@gmail.com>
2024-09-18 11:10:14 +0200

    Fix Alloy logs secret handling (#221)
    * Fix Alloy logs secret handling
    
      - Pass secret environment variables via helm chart values
        using the extraSecretEnv directive
      - Ensure old secret is deleted
    
    * add logging-secret.yaml.template
    
    * remove unused needUpdate function
    
    * use name/value
    
    * add TODO

--- pkg/resource/logging-secret/alloy-logging-secret.go
+++ pkg/resource/logging-secret/alloy-logging-secret.go
@@ -1,13 +1,69 @@
 package loggingsecret
 
 import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"text/template"
+
 	v1 "k8s.io/api/core/v1"
 
+	"github.com/Masterminds/sprig/v3"
+
+	"github.com/giantswarm/logging-operator/pkg/common"
 	loggedcluster "github.com/giantswarm/logging-operator/pkg/logged-cluster"
+	loggingcredentials "github.com/giantswarm/logging-operator/pkg/resource/logging-credentials"
 )
 
+const (
+	AlloyLokiURLEnvVarName           = "LOKI_URL"
+	AlloyTenantIDEnvVarName          = "TENANT_ID"
+	AlloyBasicAuthUsernameEnvVarName = "BASIC_AUTH_USERNAME"
+	AlloyBasicAuthPasswordEnvVarName = "BASIC_AUTH_PASSWORD" // #nosec G101
+)
+
+var (
+	//go:embed alloy/logging-secret.yaml.template
+	alloySecret         string
+	alloySecretTemplate *template.Template
+)
+
+func init() {
+	alloySecretTemplate = template.Must(template.New("logging-secret.yaml").Funcs(sprig.FuncMap()).Parse(alloySecret))
+}
+
 func GenerateAlloyLoggingSecret(lc loggedcluster.Interface, credentialsSecret *v1.Secret, lokiURL string) (map[string][]byte, error) {
+	clusterName := lc.GetClusterName()
+
+	writePassword, err := loggingcredentials.GetPassword(lc, credentialsSecret, clusterName)
+	if err != nil {
+		return nil, err
+	}
+
+	LokiURL := fmt.Sprintf(common.LokiURLFormat, lokiURL)
+	TenantID := clusterName
+	BasicAuthUsername := clusterName
+	BasicAuthPassword := writePassword
+
+	templateData := struct {
+		ExtraSecretEnv map[string]string
+	}{
+		ExtraSecretEnv: map[string]string{
+			AlloyLokiURLEnvVarName:           LokiURL,
+			AlloyTenantIDEnvVarName:          TenantID,
+			AlloyBasicAuthUsernameEnvVarName: BasicAuthUsername,
+			AlloyBasicAuthPasswordEnvVarName: BasicAuthPassword,
+		},
+	}
+
+	var values bytes.Buffer
+	err = alloySecretTemplate.Execute(&values, templateData)
+	if err != nil {
+		return nil, err
+	}
+
 	data := make(map[string][]byte)
+	data["values"] = values.Bytes()
 
 	return data, nil
 }
