85cb737c13c44ca35901769f7bbbe6105a490b3e
QuantumEnigmaa <thibaud@giantswarm.io>
2024-11-18 11:28:33 +0100

    add methods definitions to vintagewc package

--- pkg/logged-cluster/vintagewc/cluster.go
+++ pkg/logged-cluster/vintagewc/cluster.go
@@ -52,6 +52,14 @@ func (o *Object) SetLoggingAgent(loggingAgent string) {
 	o.Options.LoggingAgent = loggingAgent
 }
 
+func (o *Object) GetEventsLogger() string {
+	return o.Options.EventsLogger
+}
+
+func (o *Object) SetEventsLogger(eventsLogger string) {
+	o.Options.EventsLogger = eventsLogger
+}
+
 func (o Object) IsInsecureCA() bool {
 	return o.Options.InsecureCA
 }
