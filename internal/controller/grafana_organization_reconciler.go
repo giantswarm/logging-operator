d8b83fbf2a9f5d6d2a2aefefe7316b7fb59bb3bb
QuantumEnigmaa <thibaud@giantswarm.io>
2025-02-27 15:07:49 +0100

    add kubebuilder comments

--- internal/controller/grafana_organization_reconciler.go
+++ internal/controller/grafana_organization_reconciler.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controller
 
 import (
@@ -27,6 +43,9 @@ type GrafanaOrganizationReconciler struct {
 	Scheme                  *runtime.Scheme
 	LoggingConfigReconciler loggingconfig.Reconciler
 }
+
+//+kubebuilder:rbac:groups=observability.giantswarm.io,resources=grafanaorganizations,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=observability.giantswarm.io,resources=grafanaorganizations/finalizers,verbs=update
 
 func (g *GrafanaOrganizationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
 	logger := log.FromContext(ctx)
