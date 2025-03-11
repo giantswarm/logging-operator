a33f2429d94cfe01da22d058c1f09b7710f160be
QuentinBisson <quentin@giantswarm.io>
2025-03-11 19:31:23 +0100

    rename-from-event

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func main() {
 	var enableLogging bool
 	var loggingAgent string
 	var eventsLogger string
-	var includeEventsInNamespaces StringSliceVar
+	var includeEventsFromNamespaces StringSliceVar
 	var excludeEventsFromNamespaces StringSliceVar
 	var installationName string
 	var insecureCA bool
@@ -101,7 +101,7 @@ func main() {
 	flag.BoolVar(&enableLogging, "enable-logging", true, "enable/disable logging for the whole installation")
 	flag.StringVar(&loggingAgent, "logging-agent", common.LoggingAgentAlloy, fmt.Sprintf("select logging agent to use (%s or %s)", common.LoggingAgentPromtail, common.LoggingAgentAlloy))
 	flag.StringVar(&eventsLogger, "events-logger", common.EventsLoggerAlloy, fmt.Sprintf("select events logger to use (%s or %s)", common.EventsLoggerAlloy, common.EventsLoggerGrafanaAgent))
-	flag.Var(&includeEventsFromNamespaces, "include-events-in-namespaces", "List of namespaces to collect events from on workload clusters (if empty, collect from all namespaces)")
+	flag.Var(&includeEventsFromNamespaces, "include-events-from-namespaces", "List of namespaces to collect events from on workload clusters (if empty, collect from all namespaces)")
 	flag.Var(&excludeEventsFromNamespaces, "exclude-events-from-namespaces", "List of namespaces to exclude events from on workload clusters")
 	flag.StringVar(&installationName, "installation-name", "unknown", "Name of the installation")
 	flag.BoolVar(&insecureCA, "insecure-ca", false, "Is the management cluter CA insecure?")
@@ -180,7 +180,7 @@ func main() {
 
 	eventsLoggerConfig := eventsloggerconfig.Reconciler{
 		Client:            mgr.GetClient(),
-		IncludeNamespaces: includeEventsInNamespaces,
+		IncludeNamespaces: includeEventsFromNamespaces,
 		ExcludeNamespaces: excludeEventsFromNamespaces,
 	}
 
